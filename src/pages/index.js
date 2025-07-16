4e6d36dd8da0bad043e06d2a6ec6a107dd3b5b4f
aidilprmdta <sekaiiprojectmv@gmail.com>
2025-07-16 20:44:39 +0700

    baru

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Portofolio Aidil | Web Developer React & Tailwind</title>
+        <meta name="description" content="Portofolio pribadi Aidil Pramadita Putra, menampilkan proyek-proyek berbasis React.js, Tailwind CSS, Framer Motion, dan Node.js." />
+        <meta name="keywords" content="Portofolio Web Developer, React, Tailwind CSS, Node.js, Framer Motion, Aidil Pramadita Putra" />
+        <meta name="author" content="Aidil Pramadita Putra" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+      </Head>
+    </>
+  )
+}
