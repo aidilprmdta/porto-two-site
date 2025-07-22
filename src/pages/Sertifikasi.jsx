887a15cd8910c64290725cf6886f8d47ad9c0c19
aidilprmdta <sekaiiprojectmv@gmail.com>
2025-07-22 19:07:30 +0700

    add koment

--- src/pages/Sertifikasi.jsx
+++ src/pages/Sertifikasi.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const sertifikasiList = [
-  {
-    nama: 'Panitia Kongres',
-    penyelenggara: 'Himatif',
-    tahun: 2024,
-    deskripsi: 'Sertifikasi kepanitian kongres Himatif 2024 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/apysekdu2lyky7snwmxnb/KONGRES-Aidil-Pramadita.png?rlkey=wmzrnifolx42ldx8yhpxf173i&st=thiqxjhr',
-  },
-  {
-    nama: 'Panitia Kurmatif',
-    penyelenggara: 'Himatif',
-    tahun: 2025,
-    deskripsi: 'Sertifikasi kepanitian kurmatif Himatif 2025 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/atuiq12p3fu0pfq2dduwk/Aidil-Pramadita-Putra-3.pdf?rlkey=fd81086b777orx0uhyqxhg3ld&st=f9gmv0cz',
-  },
-  {
-    nama: 'Panitia Himatif Cup',
-    penyelenggara: 'Himatif',
-    tahun: 2025,
-    deskripsi: 'Sertifikasi kepanitian Himatif Cup 2025 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/6wnayjocvefcs9qybi8fe/Aidil-Pramadita-Putra-4.pdf?rlkey=b916krmsvude32jonq6bkahrm&st=cg13wj0d',
-  },
+  // {
+  //   nama: 'Panitia Kongres',
+  //   penyelenggara: 'Himatif',
+  //   tahun: 2024,
+  //   deskripsi: 'Sertifikasi kepanitian kongres Himatif 2024 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/apysekdu2lyky7snwmxnb/KONGRES-Aidil-Pramadita.png?rlkey=wmzrnifolx42ldx8yhpxf173i&st=thiqxjhr',
+  // },
+  // {
+  //   nama: 'Panitia Kurmatif',
+  //   penyelenggara: 'Himatif',
+  //   tahun: 2025,
+  //   deskripsi: 'Sertifikasi kepanitian kurmatif Himatif 2025 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/atuiq12p3fu0pfq2dduwk/Aidil-Pramadita-Putra-3.pdf?rlkey=fd81086b777orx0uhyqxhg3ld&st=f9gmv0cz',
+  // },
+  // {
+  //   nama: 'Panitia Himatif Cup',
+  //   penyelenggara: 'Himatif',
+  //   tahun: 2025,
+  //   deskripsi: 'Sertifikasi kepanitian Himatif Cup 2025 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/6wnayjocvefcs9qybi8fe/Aidil-Pramadita-Putra-4.pdf?rlkey=b916krmsvude32jonq6bkahrm&st=cg13wj0d',
+  // },
   {
     nama: 'Mini Bootcamp Lailatul Qoding',
     penyelenggara: 'Himatif',
@@ -30,27 +30,27 @@ const sertifikasiList = [
     deskripsi: 'Sertifikasi partisipasi dalam Mini Bootcamp Lailatul Qoding 2025 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
     link: 'https://www.dl.dropboxusercontent.com/scl/fi/bacmr83g4vs4os5plyhg5/Aidil-Pramadita-Putra.png?rlkey=jyhh1phphf6ejvz6yz43bqo0h&st=npawce33',
   },
-  {
-    nama: 'Sertifikat Peserta_Webinar File Upload Vulnerabilities Mengamankan Server dari Takeover',
-    penyelenggara: 'Institu Teknologi Tangerang Selatan (ITTS)',
-    tahun: 2024,
-    deskripsi: 'Sertifikasi partisipasi dalam webinar tentang File Upload Vulnerabilities yang diselenggarakan oleh ITTS.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/2kpbhtpkdz5gpropmck7t/002.Aidil-Pramadita_Sertifikat-Peserta_Webinar-File-Upload-Vulnerabilities-Mengamankan-Server-dari-Takeover.PDF?rlkey=oacc15pefltcq7syjpjdfb2yk&st=6mwitb58',
-  },
-  {
-    nama: 'Sertifikat Peserta_Webinar Membuat Aplikasi Kasir Sederhana dengan PHP untuk Pemula',
-    penyelenggara: 'Institu Teknologi Tangerang Selatan (ITTS)',
-    tahun: 2024,
-    deskripsi: 'Sertifikasi partisipasi dalam webinar tentang Membuat Aplikasi Kasir Sederhana dengan PHP untuk Pemula yang diselenggarakan oleh ITTS.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/k0hgpq65290mzgqd5302e/182.Aidil-Pramadita-Putra_Sertifikat-Peserta_Webinar-Membuat-Aplikasi-Kasir-Sederhana-dengan-PHP-untuk-Pemula-pages-79.pdf?rlkey=k7r60geoa4upn6n3s4lfduls5&st=i2cq5ij2',
-  },
-  {
-    nama: 'Sertifikat Peserta_Webinar Website Statis dengan HTML dan CSS Dasar',
-    penyelenggara: 'Institu Teknologi Tangerang Selatan (ITTS)',
-    tahun: 2024,
-    deskripsi: 'Sertifikasi partisipasi dalam webinar tentang Website Statis dengan HTML dan CSS Dasar yang diselenggarakan oleh ITTS.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/72494uexkwx3i2hklux4x/258.Aidil-Pramadita-Putra_Sertifikat-Peserta_Webinar-Website-Statis-dengan-HTML-dan-CSS-Dasar.pdf?rlkey=dmszt89ni319furx6a24qnjmk&st=j0bzy61c',
-  },
+  // {
+  //   nama: 'Sertifikat Peserta_Webinar File Upload Vulnerabilities Mengamankan Server dari Takeover',
+  //   penyelenggara: 'Institu Teknologi Tangerang Selatan (ITTS)',
+  //   tahun: 2024,
+  //   deskripsi: 'Sertifikasi partisipasi dalam webinar tentang File Upload Vulnerabilities yang diselenggarakan oleh ITTS.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/2kpbhtpkdz5gpropmck7t/002.Aidil-Pramadita_Sertifikat-Peserta_Webinar-File-Upload-Vulnerabilities-Mengamankan-Server-dari-Takeover.PDF?rlkey=oacc15pefltcq7syjpjdfb2yk&st=6mwitb58',
+  // },
+  // {
+  //   nama: 'Sertifikat Peserta_Webinar Membuat Aplikasi Kasir Sederhana dengan PHP untuk Pemula',
+  //   penyelenggara: 'Institu Teknologi Tangerang Selatan (ITTS)',
+  //   tahun: 2024,
+  //   deskripsi: 'Sertifikasi partisipasi dalam webinar tentang Membuat Aplikasi Kasir Sederhana dengan PHP untuk Pemula yang diselenggarakan oleh ITTS.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/k0hgpq65290mzgqd5302e/182.Aidil-Pramadita-Putra_Sertifikat-Peserta_Webinar-Membuat-Aplikasi-Kasir-Sederhana-dengan-PHP-untuk-Pemula-pages-79.pdf?rlkey=k7r60geoa4upn6n3s4lfduls5&st=i2cq5ij2',
+  // },
+  // {
+  //   nama: 'Sertifikat Peserta_Webinar Website Statis dengan HTML dan CSS Dasar',
+  //   penyelenggara: 'Institu Teknologi Tangerang Selatan (ITTS)',
+  //   tahun: 2024,
+  //   deskripsi: 'Sertifikasi partisipasi dalam webinar tentang Website Statis dengan HTML dan CSS Dasar yang diselenggarakan oleh ITTS.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/72494uexkwx3i2hklux4x/258.Aidil-Pramadita-Putra_Sertifikat-Peserta_Webinar-Website-Statis-dengan-HTML-dan-CSS-Dasar.pdf?rlkey=dmszt89ni319furx6a24qnjmk&st=j0bzy61c',
+  // },
   {
     nama: 'Smart Retargeting: Maksimalkan Lookalike Audience di Meta Ads',
     penyelenggara: 'IdWebHost',
@@ -65,13 +65,13 @@ const sertifikasiList = [
     deskripsi: 'Sertifikasi partisipasi dalam webinar "Sharing Section Black Hat vs White Hat" yang diselenggarakan oleh Alisi USR.',
     link: 'https://www.dl.dropboxusercontent.com/scl/fi/3ie8xb3isq3vozs8bul01/ALISI-Aidil-Pramadita-Putra.pdf?rlkey=x9220vhwp5raa60p9mjxsz9dw&st=jwgit446',
   },
-  {
-    nama: 'Peserta Asomatif',
-    penyelenggara: 'Himatif',
-    tahun: 2024,
-    deskripsi: 'Sertifikasi partisipasi dalam Asomatif 2024 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/eg20hy3rp8vx7u6k5bpcm/ASOMATIF-Aidil-Pramadita-Putra.png?rlkey=bc6jai6mo3b54gvh5xuigkqb4&st=0t4xqspb',
-  },
+  // {
+  //   nama: 'Peserta Asomatif',
+  //   penyelenggara: 'Himatif',
+  //   tahun: 2024,
+  //   deskripsi: 'Sertifikasi partisipasi dalam Asomatif 2024 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/eg20hy3rp8vx7u6k5bpcm/ASOMATIF-Aidil-Pramadita-Putra.png?rlkey=bc6jai6mo3b54gvh5xuigkqb4&st=0t4xqspb',
+  // },
   {
     nama: 'Devcoach 174 Flutter Bentangkan Sayap Kreativitasmu Dengan Flutter Desktop',
     penyelenggara: 'Dicoding Event',
@@ -107,13 +107,13 @@ const sertifikasiList = [
     deskripsi: 'Sertifikasi partisipasi dalam event "Mengenal Special Classes dan Collection" yang diselenggarakan oleh Dicoding.',
     link: 'https://www.dl.dropboxusercontent.com/scl/fi/w22bq3vqyf9d6ve1vswo4/devcoach-195-kotlin-101-mengenal-special-classes-dan-collection-certificate.pdf?rlkey=alt1i7c106ptb581i581bn4ee&st=l3ys9x6w',
   },
-  {
-    nama: 'Webinar: Basis Data',
-    penyelenggara: 'Universitas Pelita Bangsa',
-    tahun: 2024,
-    deskripsi: 'Sertifikasi partisipasi dalam webinar tentang Basis Data yang diselenggarakan oleh Universitas Pelita Bangsa.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/fpgjssf96ybdhhnietdbv/E-Certificate-Aidil-Pramadita-Putra.pdf?rlkey=enzf1v7twyqmyypzbsmxba1i0&st=7842ylip',
-  },
+  // {
+  //   nama: 'Webinar: Basis Data',
+  //   penyelenggara: 'Universitas Pelita Bangsa',
+  //   tahun: 2024,
+  //   deskripsi: 'Sertifikasi partisipasi dalam webinar tentang Basis Data yang diselenggarakan oleh Universitas Pelita Bangsa.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/fpgjssf96ybdhhnietdbv/E-Certificate-Aidil-Pramadita-Putra.pdf?rlkey=enzf1v7twyqmyypzbsmxba1i0&st=7842ylip',
+  // },
   {
     nama: 'Idcamp Alumni Dialogue 3 Unlock Your Potential Menjadi Instruktur Fasilitator Dan Speaker Handal Sambil Bekerja',
     penyelenggara: 'Indosat Ooredoo Hutchison Digital Camp',
@@ -156,27 +156,27 @@ const sertifikasiList = [
     deskripsi: 'Sertifikasi partisipasi dalam event "FROM ZERO TO HACKER',
     link: 'https://www.dl.dropboxusercontent.com/scl/fi/onzi7yayy61leqy01k67k/JH-Sertifikat-Event-FROM-ZERO-TO-HACKER-halaman-14.pdf?rlkey=lwnltm59a5ozhb7rj3xxejs7w&st=lcoodjua',
   },
-  {
-    nama: 'Seminar Nasional Public Speaking',
-    penyelenggara: 'Speaking Official',
-    tahun: 2024,
-    deskripsi: 'Sertifikasi partisipasi dalam seminar Nasional Public Speaking sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/ztsosfc9l7s7jk03sppez/PUBLIC-SPEAKING-Aidil-Pramadita-Putra.jpg?rlkey=gjbgustcvgxu1363gpzg3m4em&st=h4320s8g',
-  },
-  {
-    nama: 'Reschedule Idcamp Alumni Dialogue 4 Failing Forward Turning Setbacks Into Growth In Tech',
-    penyelenggara: 'Indosat Ooredoo Hutchison Digital Camp',
-    tahun: 2025,
-    deskripsi: 'Sertifikasi partisipasi dalam idcamp Alumni Dialogue 4 yang berfokus pada mengubah kemunduran menjadi pertumbuhan di bidang teknologi.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/8ybh77eduqojhf6np9rnb/reschedule-idcamp-alumni-dialogue-4-failing-forward-turning-setbacks-into-growth-in-tech-certificate.pdf?rlkey=f2cplm0c23q1odunjnsj24z7p&st=2eut0n78',
-  },
-  {
-    nama: 'Webinar Pengembangan Diri ',
-    penyelenggara: 'Teknik Informatika UIN Suska Riau',
-    tahun: 2025,
-    deskripsi: 'Sertifikasi partisipasi dalam webinar Pengembangan Diri yang diselenggarakan oleh Teknik Informatika UIN Suska Riau.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/pfpkqle8z7z0rhpwsx37i/Sertifikat-Aidil-Pramadita-Putra.pdf?rlkey=8y505bt1f2fgrolfgszizct63&st=p1qi2uzd',
-  },
+  // {
+  //   nama: 'Seminar Nasional Public Speaking',
+  //   penyelenggara: 'Speaking Official',
+  //   tahun: 2024,
+  //   deskripsi: 'Sertifikasi partisipasi dalam seminar Nasional Public Speaking sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/ztsosfc9l7s7jk03sppez/PUBLIC-SPEAKING-Aidil-Pramadita-Putra.jpg?rlkey=gjbgustcvgxu1363gpzg3m4em&st=h4320s8g',
+  // },
+  // {
+  //   nama: 'Reschedule Idcamp Alumni Dialogue 4 Failing Forward Turning Setbacks Into Growth In Tech',
+  //   penyelenggara: 'Indosat Ooredoo Hutchison Digital Camp',
+  //   tahun: 2025,
+  //   deskripsi: 'Sertifikasi partisipasi dalam idcamp Alumni Dialogue 4 yang berfokus pada mengubah kemunduran menjadi pertumbuhan di bidang teknologi.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/8ybh77eduqojhf6np9rnb/reschedule-idcamp-alumni-dialogue-4-failing-forward-turning-setbacks-into-growth-in-tech-certificate.pdf?rlkey=f2cplm0c23q1odunjnsj24z7p&st=2eut0n78',
+  // },
+  // {
+  //   nama: 'Webinar Pengembangan Diri ',
+  //   penyelenggara: 'Teknik Informatika UIN Suska Riau',
+  //   tahun: 2025,
+  //   deskripsi: 'Sertifikasi partisipasi dalam webinar Pengembangan Diri yang diselenggarakan oleh Teknik Informatika UIN Suska Riau.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/pfpkqle8z7z0rhpwsx37i/Sertifikat-Aidil-Pramadita-Putra.pdf?rlkey=8y505bt1f2fgrolfgszizct63&st=p1qi2uzd',
+  // },
   {
     nama: 'IDCH Talk Vol 51. Tren Website 2025',
     penyelenggara: 'IdCloudHost',
@@ -184,27 +184,27 @@ const sertifikasiList = [
     deskripsi: 'Sertifikasi partisipasi dalam IDCH Talk Vol 51 yang membahas tren website di tahun 2025.',
     link: 'https://www.dl.dropboxusercontent.com/scl/fi/kq47hidesj41akdja0m5i/Sertifikat-Aidil-Pramadita-Putra.pdf?rlkey=u1szu58pws49x8nmkjse3adia&st=0j4r7433',
   },
-  {
-    nama: 'Kuliah Umum: Startegi Adaptasi Mahasiswa Baru Dalam Meraih Kesuksesan Studi',
-    penyelenggara: 'UIN Suska Riau',
-    tahun: 2024,
-    deskripsi: 'Sertifikasi partisipasi dalam kuliah umum yang membahas strategi adaptasi mahasiswa baru dalam meraih kesuksesan studi.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/0dmjvpyxm7yx5duh4rdwt/Sertifikat-Kuliah-Umum-Univesitas-2024-Sesi-Siang.pdf?rlkey=m8lrhgah3y82yxpjgwpxlshaj&st=tzonl3re',
-  },
-  {
-    nama: 'Seminar: Analisis Data Berbasis SQL Untuk Pengambilan Keputusan',
-    penyelenggara: 'Universitas Pelita Bangsa',
-    tahun: 2024,
-    deskripsi: 'Sertifikasi partisipasi dalam seminar yang membahas analisis data berbasis SQL untuk pengambilan keputusan.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/kj1rrgnzu8bsrkhz1fbqs/Sertifikat-Seminar-Online-Manajemen-Basis-Data-Aidil-Pramadita-Putra.pdf?rlkey=3r4rom4vwkayb9z69vvvxomwl&st=aa766oje',
-  },
-  {
-    nama: 'Peserta Simatif',
-    penyelenggara: 'Himatif',
-    tahun: 2024,
-    deskripsi: 'Sertifikasi partisipasi dalam Simatif 2024 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
-    link: 'https://www.dl.dropboxusercontent.com/scl/fi/jwyzwgq9znm10xrrwlwal/SIMATIF-Aidil-Pramadita-Putra.png?rlkey=y0jrr12y67atfp2jhua22dreo&st=9nqsv6pb',
-  },
+  // {
+  //   nama: 'Kuliah Umum: Startegi Adaptasi Mahasiswa Baru Dalam Meraih Kesuksesan Studi',
+  //   penyelenggara: 'UIN Suska Riau',
+  //   tahun: 2024,
+  //   deskripsi: 'Sertifikasi partisipasi dalam kuliah umum yang membahas strategi adaptasi mahasiswa baru dalam meraih kesuksesan studi.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/0dmjvpyxm7yx5duh4rdwt/Sertifikat-Kuliah-Umum-Univesitas-2024-Sesi-Siang.pdf?rlkey=m8lrhgah3y82yxpjgwpxlshaj&st=tzonl3re',
+  // },
+  // {
+  //   nama: 'Seminar: Analisis Data Berbasis SQL Untuk Pengambilan Keputusan',
+  //   penyelenggara: 'Universitas Pelita Bangsa',
+  //   tahun: 2024,
+  //   deskripsi: 'Sertifikasi partisipasi dalam seminar yang membahas analisis data berbasis SQL untuk pengambilan keputusan.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/kj1rrgnzu8bsrkhz1fbqs/Sertifikat-Seminar-Online-Manajemen-Basis-Data-Aidil-Pramadita-Putra.pdf?rlkey=3r4rom4vwkayb9z69vvvxomwl&st=aa766oje',
+  // },
+  // {
+  //   nama: 'Peserta Simatif',
+  //   penyelenggara: 'Himatif',
+  //   tahun: 2024,
+  //   deskripsi: 'Sertifikasi partisipasi dalam Simatif 2024 sebagai bentuk pengakuan atas kontribusi dalam organisasi.',
+  //   link: 'https://www.dl.dropboxusercontent.com/scl/fi/jwyzwgq9znm10xrrwlwal/SIMATIF-Aidil-Pramadita-Putra.png?rlkey=y0jrr12y67atfp2jhua22dreo&st=9nqsv6pb',
+  // },
 ];
 
 const cardVariants = {
@@ -239,7 +239,7 @@ const Sertifikasi = () => {
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.6, delay: 0.1 }}
       >
-        Sertifikasi & Penghargaan
+        Sertifikat Event
       </motion.h2>
 
       <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full max-w-6xl">
